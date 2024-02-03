e2ca8542ebb4695ab4fd66b5e8f08f2a994e41f3
408-16 <>
2024-02-03 19:44:53 +0900

    agdj

---
+++ 20231118_function_永井拓斗.cpp
@@ -0,0 +1,12 @@
+#include"20231118_header_‰iˆä‘ñ“l.h"
+
+int ramdom(int playercrad[], int enemycrad[])
+{
+    for (int i = 0; crad < i; i++)
+    {
+        playercrad[i]=rand()%3;
+        enemycrad[i] = rand() % 3;
+
+
+    }
+}
